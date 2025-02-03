b98858adec25a2274bcda9c0a21c981681e98b8f
Chirag Nanda <chiragnanda@Chirags-MacBook-Air.local>
2025-02-04 01:12:48 +0530

    cors error commit 1

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ import { query } from './suggest.js';
 
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 const app= express();
 
 dotenv.config();
-app.use(cors());
-app.use(cors({origin : 'https://chefgptbackend.vercel.app'}))
+
+app.use(cors({origin : 'https://mychefgpt.vercel.app'}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
